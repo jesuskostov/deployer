4b9bb2d0fa99d2368088178227f3ef2419db46b9
jesuskostov <jesuskostov@gmail.com>
2022-09-29 16:34:07 +0300

    bg

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -379,7 +379,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
 @import url("https://fonts.googleapis.com/css2?family=Manrope:wght@300;500;800&family=Nabla&display=swap");
 
 /* Font face */
